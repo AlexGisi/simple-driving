99f61cc1b289c392eaf018355734677ea19c2d42
Alex Gisi <apgisi11@gmail.com>
2024-08-03 16:05:19 -0500

    Project structure

---
+++ simple_driving_planning/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+setup_args = generate_distutils_setup(
+    packages=['simple_driving_planning'],  # Python packages to install
+    package_dir={'': 'src'},         # Directory where Python packages are located
+    scripts=['scripts/planning_node.py']     # Scripts to be installed
+)
+
+setup(**setup_args)
